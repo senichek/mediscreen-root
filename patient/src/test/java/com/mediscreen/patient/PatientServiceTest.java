448a6ceb1675768ee0a2888faf0da17f11374a2d
Olexiy <senichek@gmail.com>
2022-01-09 16:57:52 +0100

    Added Profile in Patient subproject

--- patient/src/test/java/com/mediscreen/patient/PatientServiceTest.java
+++ patient/src/test/java/com/mediscreen/patient/PatientServiceTest.java
@@ -12,8 +12,10 @@ import com.mediscreen.patient.services.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class PatientServiceTest {
     
     @Autowired
